44163fbf1e81e9a40264314fb8c0e89f69693b4f
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-25 14:04:13 -0700

    Fix typo

--- app/models.py
+++ app/models.py
@@ -14,7 +14,7 @@ class User(db.Model):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
-    bots = db.relationship("Bot")
+    # bots = db.relationship("Bot")
 
     def to_dict(self):
         return {
@@ -35,36 +35,36 @@ class User(db.Model):
         if len(password) < 8 or len(password) > 50:
             raise AssertionError(
                 'Password must be between 8 and 50 characters')
-        self.hashedPassword = generate_password_hash(password)
+        self.hashed_password = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
 
 
-class Bot(db.Model):
-    __tablename__ = 'bots'
+# class Bot(db.Model):
+#     __tablename__ = 'bots'
 
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
-    name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
-    developer_token = db.Column(db.String(100))
-    # nullable because presumably we want users to be able
-    # to create a draft bot before they have a developer token
+#     id = db.Column(db.Integer, primary_key=True)
+#     user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
+#     name = db.Column(db.String(100), nullable=False)
+#     description = db.Column(db.Text)
+#     developer_token = db.Column(db.String(100))
+#     # nullable because presumably we want users to be able
+#     # to create a draft bot before they have a developer token
 
-    owner = db.relationship("User")
-    rules = db.relationship("Rules")
+#     owner = db.relationship("User")
+#     rules = db.relationship("Rules")
 
 
-class Rule(db.Model):
-    __tablename__ = 'rules'
+# class Rule(db.Model):
+#     __tablename__ = 'rules'
 
-    id = db.Column(db.Integer, primary_key=True)
-    bot_id = db.Column(db.Integer, db.ForeignKey("Bot.id"), nullable=False)
-    prefix = db.Column(db.String(100))
-    # nullable because we want to make it possible to create prefix-less rules
-    content = db.Column(db.Text, nullable=False)
-    # this is "text" rather than a string because it could be
-    # very long in some cases
+#     id = db.Column(db.Integer, primary_key=True)
+#     bot_id = db.Column(db.Integer, db.ForeignKey("Bot.id"), nullable=False)
+#     prefix = db.Column(db.String(100))
+#     # nullable because we want to make it possible to create prefix-less rules
+#     content = db.Column(db.Text, nullable=False)
+#     # this is "text" rather than a string because it could be
+#     # very long in some cases
 
-    bot = db.relationship("Bot")
+#     bot = db.relationship("Bot")
