e742419d8c3b08560733d6420279d49b139ed561
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-25 13:24:30 -0700

    Add home page

---
+++ client/src/pages/HomePage.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, loadUser } from '../store/auth';
+import { useHistory } from 'react-router-dom';
+
+export function HomePage({ logoutDispatcher, user }) {
+  const history = useHistory();
+  const handleLogout = e => {
+    e.preventDefault();
+    logoutDispatcher();
+  };
+
+  const handleLogin = e => {
+    e.preventDefault();
+    history.push('/login');
+  };
+
+  const handleSignup = e => {
+    e.preventDefault();
+    history.push('/register');
+  };
+
+  return (
+    <div>
+      <h1>All-A-Bot</h1>
+      <div>
+        {user ? (
+          <button onClick={handleLogout}>Logout</button>
+        ) : (
+          <div>
+            <button onClick={handleLogin}>Log In</button>
+            <button onClick={handleSignup}>Sign Up</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default function HomePageContainer() {
+  const dispatch = useDispatch();
+  const logoutDispatcher = () => dispatch(logout());
+  const loadUserDispatcher = () => dispatch(loadUser());
+  const user = useSelector(state => state.auth.user);
+
+  return <HomePage logoutDispatcher={logoutDispatcher} user={user} />;
+}
