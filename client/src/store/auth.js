454d623e719363c97dafac83c43706c6f5fbc46b
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-25 13:27:39 -0700

    Fix bug

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -73,7 +73,7 @@ export const login = (email, password) => async dispatch => {
   }
 };
 
-export const logout = () => async dispatch => {
+export const logout = () => async (dispatch, getState) => {
   const {
     auth: { token },
   } = getState();
