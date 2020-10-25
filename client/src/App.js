4977a27098a4e3f132f72c605b96e9aecb564fd7
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-25 13:22:08 -0700

    Set up

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
 
 import UserList from './components/UsersList';
-
+import PagesContainer from './pages/Pages';
 
 function App() {
-
   return (
     <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/users">
-                <UserList />
-            </Route>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to='/' activeclass='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/users' activeclass='active'>
+              Users
+            </NavLink>
+          </li>
+        </ul>
+        <PagesContainer />
+      </nav>
+      <Switch>
+        <Route path='/users'>
+          <UserList />
+        </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
-        </Switch>
+        <Route path='/'>
+          <h1>My Home Page</h1>
+        </Route>
+      </Switch>
     </BrowserRouter>
   );
 }
