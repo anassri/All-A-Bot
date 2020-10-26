ad2b20b33263819c4d45ef840830079d43ae6b5a
sholt20 <samuelholt123@gmail.com>
2020-10-26 12:51:38 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -48,7 +48,7 @@ class Bot(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("User.id"), nullable=False)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text)
-    isDraft = db.Column(db.Boolean, nullable=False)
+    is_draft = db.Column(db.Boolean, nullable=False)
     # developer_token = db.Column(db.String(100))
     # nullable because presumably we want users to be able
     # to create a draft bot before they have a developer token
