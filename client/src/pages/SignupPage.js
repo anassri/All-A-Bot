b851f67d38251bbe3854f22ac59e8859766d23c7
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-27 13:57:47 -0700

    Style signup form

--- client/src/pages/SignupPage.js
+++ client/src/pages/SignupPage.js
@@ -2,10 +2,44 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { signup, loadUser } from '../store/auth';
+import { makeStyles } from '@material-ui/core';
+import { Box, TextField, Button, Checkbox, Typography, Container, Paper } from '@material-ui/core';
 
-import { Box, TextField, Button, Checkbox } from '@material-ui/core';
+const useStyle = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    marginTop: 10,
+  },
+  paper: {
+    height: '60vh',
+    padding: '55px 65px',
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  bots: {
+    textAlign: 'left',
+  },
+  bot: {
+    display: 'flex',
+    borderBottom: '1px solid rgba(232,232,232,0.3)',
+  },
+  action: {
+    paddingTop: 15,
+    paddingLeft: 15,
+  },
+  content: {
+    display: 'flex',
+    alignItems: 'flex-end',
+    marginBottom: 5,
+    paddingLeft: 20,
+  },
+}));
 
 export const SignupPage = ({ user, signupDispatch, loadUserDispatch }) => {
+  const classes = useStyle();
+
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
@@ -47,21 +81,65 @@ export const SignupPage = ({ user, signupDispatch, loadUserDispatch }) => {
           <div>{error}</div>
         ))}
       </Box>
-      <form onSubmit={handleSubmit}>
-        <TextField value={email} type='email' onChange={e => setEmail(e.target.value)} label='Email'></TextField>
-        <TextField value={username} onChange={e => setUsername(e.target.value)} label='Username'></TextField>
-        <TextField
-          value={password}
-          type='password'
-          onChange={e => setPassword(e.target.value)}
-          label='Password'></TextField>
-        <TextField
-          value={confirmPassword}
-          type='password'
-          onChange={e => setConfirmPassword(e.target.value)}
-          label='Confirm Password'></TextField>
-        <Button type='submit'>Sign Up</Button>
-      </form>
+
+      <div className={classes.root}>
+        <Container maxWidth='lg' className='paper-container'>
+          <Paper className={classes.paper}>
+            <div className='signup-container'>
+              <Typography variant='h4' component='h2' style={{ fontWeight: 'bold', color: 'white' }}>
+                Create an account
+              </Typography>
+            </div>
+            <br></br>
+            <br></br>
+
+            <Container>
+              <form onSubmit={handleSubmit}>
+                <Box style={{ display: 'flex', flexDirection: 'column' }}>
+                  <TextField
+                    value={email}
+                    type='email'
+                    onChange={e => setEmail(e.target.value)}
+                    variant='outlined'
+                    label='Email'></TextField>
+                  <br></br>
+                  <TextField
+                    value={username}
+                    onChange={e => setUsername(e.target.value)}
+                    variant='outlined'
+                    label='Username'></TextField>
+                  <br></br>
+                  <TextField
+                    value={password}
+                    type='password'
+                    variant='outlined'
+                    onChange={e => setPassword(e.target.value)}
+                    label='Password'></TextField>
+                  <br></br>
+                  <TextField
+                    value={confirmPassword}
+                    type='password'
+                    variant='outlined'
+                    onChange={e => setConfirmPassword(e.target.value)}
+                    label='Confirm Password'></TextField>
+                  <br></br>
+                  <Button variant='contained' color='primary' type='submit'>
+                    Create
+                  </Button>
+                  <br></br>
+                  <Typography variant='subtitle1' component='h3' style={{ textAlign: 'left' }}>
+                    <span>
+                      <a style={{ color: '#914A84', fontWeight: 'bold' }} href='/login'>
+                        Already have an account?
+                      </a>
+                    </span>
+                  </Typography>
+                </Box>
+              </form>
+            </Container>
+          </Paper>
+        </Container>
+      </div>
     </Box>
   );
 };
