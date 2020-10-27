79f4a824a2f738a12fb64ce08958346b30ba2fa3
sholt20 <samuelholt123@gmail.com>
2020-10-27 12:37:19 -0400

    Added some comments

--- client/src/utils/fileSaver.js
+++ client/src/utils/fileSaver.js
@@ -1,11 +1,13 @@
 import { saveAs } from 'file-saver'
 
+// Function that simple takes in a string, turns it into a blob, then runs the saveAs function to initiate download
 function fileDownload(file) {
     const blob = new Blob([file], {type: "text/plain;charset=utf-8"})
 
     saveAs(blob, 'bot.js')
 }
 
+// Blob for the package.json that will be needed to run the bot
 const packageBlob = new Blob([`{
     "name": "template-bot",
     "version": "1.0.0",
