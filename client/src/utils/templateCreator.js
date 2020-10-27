0977a8184e9655c86f8b0a5ae81afe1c5a05b543
sholt20 <samuelholt123@gmail.com>
2020-10-27 11:52:10 -0400

    Added comments to explain various functions

--- client/src/utils/templateCreator.js
+++ client/src/utils/templateCreator.js
@@ -1,4 +1,4 @@
-
+// Basic stuff required at the start of each file
 const fileStart = `
 const Discord = require('discord.js');
 const client = new Discord.Client();
@@ -10,13 +10,15 @@ client.once('ready', () => {
 })
 
 `
-
+// String form of discord.js login function
 const loginString = (token) => {
     `const login = async (${token}) => { await client.login(${token})}`
 }
 
+// Random string generator, used to ensure that generated objects won't create naming conflicts
 const randStringMaker = () => Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
+// Builds string represesntations of non-message event listeners
 function nonMessageEventsBuidler(events) {
     let nonMessageEvents = ``
 
@@ -27,19 +29,24 @@ function nonMessageEventsBuidler(events) {
     return nonMessageEvents
 }
 
+// Creates string representations of the command objects necessary for discord.js to recognize them as commands, and also sets them to the bot/client
 function commandObjectsBuilder(objList) {
     let commandObjects = ``
 
     objList.forEach(cmd => {
         const varName = cmd.name + randStringMaker()
         if (cmd.trigger.includesOrStarts && cmd.trigger.includesOrStarts === 'starts') {
+            // This will fire off if the command/rule's trigger is a starts-with trigger, and its response type is that of send (this is largely for prefixed commands)
             if (cmd.response.send) {
                 commandObjects += `\n${varName} = {name: ${cmd.name}, description: ${cmd.description}, async execute(message, args) {${basicResponseBuilder(cmd.response.send)}}}\nclient.commands.set(varName.name, varName)`
             }
+            // Add else if here for events other than send for when a prefixed command has other actions such as kick, delete, or ban
         } else if(cmd.trigger.includesOrStarts && cmd.trigger.includesOrStarts === 'includes') {
+            // This will fire off if the command/rule's trigger is includes, and its response type is that of send
             if (cmd.response.send) {
                 commandObjects += `\n${varName} = {name: ${cmd.name}, description: ${cmd.description}, async execute(message, args) {${basicResponseBuilder(cmd.response.send)}}}\nclient.commands.set(varName.name, varName)`
             }
+            // Add else ifs here for events other than send for when a matching substring is found, like kick, delete, or ban
         } else {
             // Add command object creation for any other types we may add (if they don't get put into else ifs as needed)
         }
@@ -48,13 +55,14 @@ function commandObjectsBuilder(objList) {
     return commandObjects
 }
 
+// Basic helper function for creating basic send message commands
 function basicResponseBuilder(response) {
     return `message.channel.send('${response}')`
 }
 
 
 
-
+// Builds the switch case inside a conditional for messages that start with a prefix
 function switchCaseWithPrefixBuilder(prefix, commands) {
     let ifStatement = `    if (!message.content.startsWith('${prefix}')) {\n`
 
@@ -79,6 +87,7 @@ function switchCaseWithPrefixBuilder(prefix, commands) {
     return fullBlock
 }
 
+// Builds all the message listeners in if else if statements for events that are meant to trigger when a message contains certain substring(s)
 function substringMatcher(commands) {
     let substringCheck = `        const checkContains = (message, term) => message.content.contains(term)\n`
 
@@ -96,6 +105,7 @@ function substringMatcher(commands) {
     return fullBlock
 }
 
+// Assembles the final message event handler
 function messageEventAssembler(prefix, prefixedCommands, unprefixedCommands) {
     let messageEventStart = `client.on('message', message => {\n    if (message.author.bot) return;\n`
 
@@ -106,4 +116,7 @@ function messageEventAssembler(prefix, prefixedCommands, unprefixedCommands) {
     return messageEventStart + switches + messageEventEnd
 }
 
+// Testing console.log
 console.log( fileStart + messageEventAssembler('!', [{ name: 'Hello' }, { name: 'Goodbye' }], [{ name: 'olleH'}, { name: 'eybdooG' }]))
+
+// More will be added here later for final assembly and converting string to a js file for download
