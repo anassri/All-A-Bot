66969cd7e6334149818f66f1951ca46ccb25925e
Ivan Roth <ivan.h.roth@gmail.com>
2020-10-27 13:19:48 -0700

    fix rule preloading and add response form

--- client/src/pages/EditBot.js
+++ client/src/pages/EditBot.js
@@ -5,22 +5,20 @@ import { loadBot } from '../store/bots'
 
 function EditBot({bot, botId, user}) {
 
-    const BLANK_RULE = { name: "", prefix: "", content: { trigger: {type: ""}, response: [{}] } };
+    const BLANK_RULE = { name: "", prefix: "", content: { trigger: {type: "", details: { string: "" }}, response: [{type: "", details: { string: "" }}] } };
 
     const [botName, setBotName] = useState("");
     const [rules, setRules] = useState([BLANK_RULE]);
     const [anchorEl, setAnchorEl] = useState(null);
+    const [responseAnchor, setResponseAnchor] = useState(null);
 
     useEffect(() => {
         console.log(bot);
         console.log(bot.rules);
         console.log(rules);
-        if (rules[0] === BLANK_RULE || rules.length === 0){
+        if (bot.rules.length === 0 || rules[0] !== bot.rules[0]){
             setRules(bot.rules);
             console.log(rules);
-            // for (let i=0; i<bot.rules.length; i++){
-            //     rules[i].content = JSON.parse(bot.rules[i].content);
-            // }
         }
         if (botName === "") setBotName(bot.name);
     })
@@ -49,6 +47,7 @@ function EditBot({bot, botId, user}) {
             <form>
                 <TextField label="Prefix" value={rules[i].prefix} onChange={e => setRule(i, {...rules[i], prefix: e.target.value})}></TextField>
                 <div>
+                {/* this is the div which contains the trigger form. the next div down is the response form. */}
                 <Button onClick={e => setAnchorEl(e.currentTarget)}>Trigger</Button>
                 <Menu
                     keepMounted
@@ -59,11 +58,43 @@ function EditBot({bot, botId, user}) {
                     onClose={() => setAnchorEl(null)}
                 >
                     <MenuItem onClick={e => {
-                        setRule(i, {...rules[i], content: { ...rules[i].content, trigger: {type: "message"} }})
+                        setRule(i, {...rules[i], content: { ...rules[i].content, trigger: {...rules[i].content.trigger, type: "message"} }})
+                        // this is how we have to modify a rule - it's important to spread all of the intermediate nested objects into the new rule to make sure
+                        // that nothing is being overwritten.
+                        // we can't just modify the rule directly because it is part of the state.
+                        // we follow this same pattern below when we are setting the string in the trigger details using the "trigger message" field.
                     }}>Message</MenuItem>
+
                 </Menu>
+                {rules[i].content.trigger.type === "message" ? <Box>
+                    <TextField label="Trigger message: "
+                               value={rules[i].content.trigger.details.string}
+                               onChange={e => setRule(i, {...rules[i], content: {...rules[i].content, trigger: {...rules[i].content.trigger, details: { ...rules[i].content.trigger.details, string: e.target.value }}}})} />
+                    </Box> : <></>}
                 </div>
-                {rules[i].content.trigger.type === "message" ? <Box><TextField label="Trigger: "></TextField><TextField label="Response: "></TextField></Box> : <></>}
+
+
+                <div>
+                <Button onClick={e => setResponseAnchor(e.currentTarget)}>Response</Button>
+                <Menu
+                    keepMounted
+                    anchorEl={responseAnchor}
+                    open={Boolean(responseAnchor)}
+                    label="On"
+                    className="response-menu"
+                    onClose={() => setResponseAnchor(null)}
+                >
+                    <MenuItem onClick={e => {
+                        setRule(i, {...rules[i], content: { ...rules[i].content, response: [{...rules[i].content.response[0], type: "message"}] }})
+                    }}>Message</MenuItem>
+
+                </Menu>
+                {rules[i].content.response[0].type === "message" ? <Box>
+                    <TextField label="Response message: "
+                               value={rules[i].content.response[0].details.string}
+                               onChange={e => setRule(i, {...rules[i], content: {...rules[i].content, response: [{...rules[i].content.response[0], details: { ...rules[i].content.response[0].details, string: e.target.value }}]}})} />
+                    </Box> : <></>}
+                </div>
             </form>
         </>
     )}
