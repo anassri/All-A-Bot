b1ba876e560f9cde9d833d97e431c36b0325e712
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-29 09:32:34 -0700

    Add redirect page

---
+++ client/src/pages/Redirect.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { useHistory, useParams, useLocation } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logInWithDiscord } from '../store/auth';
+
+export const Redirect = ({ code, user, logInWithDiscordDispatch }) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    logInWithDiscordDispatch(code);
+  });
+
+  useEffect(() => {
+    if (user) history.push('/dashboard');
+  }, user);
+  return <></>;
+};
+
+export default function RedirectContainer() {
+  const dispatch = useDispatch();
+  const code = useLocation().search.match(/(?<=code=)\w+/)[0];
+  const user = useSelector(state => state.auth.user);
+  const logInWithDiscordDispatch = code => dispatch(logInWithDiscord(code));
+  return <Redirect code={code} user={user} logInWithDiscordDispatch={logInWithDiscordDispatch} />;
+}
