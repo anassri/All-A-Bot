b4690df8a60e5af5f811bedfedbd2ba4161c335b
Matt Ramotar <matt.ramotar@jhu.edu>
2020-10-30 09:16:51 -0700

    Create backend route for user bookmarks

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, jsonify
-from app.models import User
+from flask import Blueprint, jsonify, request
+from app.models import db, User, User_Bookmark
 
 user_routes = Blueprint('users', __name__, url_prefix='/api/users')
 
@@ -8,3 +8,18 @@ user_routes = Blueprint('users', __name__, url_prefix='/api/users')
 def index():
     response = User.query.all()
     return {"users": [user.to_dict() for user in response]}
+
+
+@user_routes.route('/bots/bookmarks', methods=['POST'])
+def user_bookmarks():
+    incoming = request.get_json()
+    userBookmark = User_Bookmark(
+        userId=incoming['userId'], botId=incoming['botId'])
+
+    db.session.add(userBookmark)
+    db.session.commit()
+
+    user = User.query.get(incoming['userId'])
+    return jsonify(
+        bookmarks=[bookmark.to_dict() for bookmark in user.bookmarks],
+        user=user.to_dict())
